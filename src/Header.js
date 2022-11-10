6037fb82634efffc023b4bf3c84c8052df6bf43a
Basim Rauf <basimrauf@gmail.com>
2022-11-10 14:44:10 +0530

    updated shop name

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <div className="bg-black border-white text-lg text-white">
       <img className="h-20 w-20 inline-block" src={logo} alt="logo" />
-      <h1 className="font-xl font-bold inline-block">Shopperoo</h1>
+      <h1 className="font-xl font-bold inline-block">WeCart</h1>
     </div>
   );
 }
